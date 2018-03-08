d5ab8bf89e04fa5e27c8f0c88d9a014d65d12f78
zhaopeng <874391057@qq.com>
2018-03-08 19:43:03 +0800

    合并页面

--- js/list-sh.js
+++ js/list-sh.js
@@ -1,10 +1,17 @@
 $(function(){
-	var num = "";
-	var linum = $('.listpage li').length;
+var contentpost = 	'js/content.json'; //内容接口
+var listpost = 	'js/list.json';		//列表接口
+var uploadpost = "";				//审核通过提交接口
+var num = 0;
+function listpagebtn(linum){	
+	
+	var linum =linum ;
+
 	$('.listpage li').click(function(){
 		$(this).addClass('cli').siblings().removeClass('cli');
 		num =$(this).index();
-
+		var numm= num+1;
+		getlist(numm);
 	});
 	$('.onepage').click(function(){
 		$('.listpage li').eq(0).addClass('cli').siblings().removeClass('cli');
@@ -12,12 +19,17 @@ $(function(){
 		if(linum>5){
 			for(var i=5; i<linum; i++){
 				$('.listpage li').eq(i).hide();
-			}
+				
+				}
 			for(var i=0; i<5; i++){
 				$('.listpage li').eq(i).show();
 			}
 		}
+		var numm= num+1;
+		getlist(numm);
 	});
+	
+	
 	$('.lastpage').click(function(){
 		num = linum-1;
 		
@@ -31,7 +43,8 @@ $(function(){
 				$('.listpage li').eq(i).show();
 			}
 		}
-		console.log(num);
+		var numm= num+1;
+		getlist(numm);
 	});
 	
 	$('.prvepage').click(function(){
@@ -41,9 +54,13 @@ $(function(){
 		$('.listpage li').eq(num).show();
 		$('.listpage li').eq(num+5).hide();
 		$('.listpage li').eq(num).addClass('cli').siblings().removeClass('cli');
+		var numm= num+1;
+		getlist(numm);
 	});
 	$('.nextpage').click(function(){
+		
 		if(num!=linum-1){
+			console.log( num, linum );
 			num++;
 		}
 		$('.listpage li').eq(num).show();
@@ -51,8 +68,17 @@ $(function(){
 			$('.listpage li').eq(num-5).hide();
 		}
 		$('.listpage li').eq(num).addClass('cli').siblings().removeClass('cli');
+		var numm= num+1;
+		getlist(numm);
+			
 	});
-	
+
+
+};
+
+
+
+
 $('.list-dede-bg').click(function(){
 	$('.list-dede').hide();
 	$('.map').hide();
@@ -68,18 +94,21 @@ $('.listclose').click(function(){
 });
 $('.mapbtn').click(function(){
 	$('.map').show();
-	map();
+
 })
 
-function map(){
-	
+function map(dizhi){
+	$('.map').css({
+		width:'400px',
+		height:'400px'
+	})
 				var map = new BMap.Map("allmap");
 				var point = new BMap.Point(116.331398,39.897445);
 				map.centerAndZoom(point,12);
 				// 创建地址解析器实例
 				var myGeo = new BMap.Geocoder();
-				// 将地址解析结果显示在地图上,并调整地图视野
-				myGeo.getPoint("杭州市江干区笕丁路168号", function(point){
+				
+				myGeo.getPoint(dizhi, function(point){
 				if (point) {
 				map.centerAndZoom(point, 16);
 				map.addOverlay(new BMap.Marker(point));
@@ -89,24 +118,13 @@ function map(){
 				}, "北京市");
 };
 
-$('.shenhe').click(function(){
-	
-	$('.list-dede').show();
-});
-if(linum>5){
-	for(var i = 5; i<linum; i++){
-		$('.listpage li').eq(i).hide();
-	}
-}
+
+
 
 $(document).click(function(){
 	$('.openimg').hide();
 });
-$('.list-de-right img').click(function(event){
-	event.stopPropagation();
-	$('.openimg').show();
-	$('.openimg img').attr('src',$(this).attr('src'));
-});
+
 $('.refuse-close').click(function(){
 		$('.refuse-tc').hide();
 });
@@ -114,19 +132,138 @@ $('.refuse').click(function(){
 	$('.refuse-tc').show();
 });
 
-$.ajax({
-	type:'GET',
-	url:'js/list.json',
+function getlist(page){
+	$.ajax({
+	type:'POST',
+	url:listpost,//获取列表
+	timeout:10000,
 	data:{
+		pageNo:page,
+		pageSize:10,
+	},success:function(data){
+			$('.listcontent').html("");
+		$.each(data.list,function(index){
+			$('.listcontent').append('<div class="listlist"><ul><li>'+data.list[index].shopName+'</li><li>'+data.list[index].shopCode+'</li><li class="shenh shenhe">立即审核</li><li class="listid">'+ data.list[index].id +'</li></ul></div>');	
+		});	
 		
+		$('.shenh').click(function(){
+			$('.list-dede').show();		
+		});
+	},complete : function(XMLHttpRequest,status){ 
+　　　　if(status=='timeout'){
+ 　　　　　 ajaxTimeoutTest.abort();
+　　　　　  alert("超时");
+　　　　		}
+　　		}
+		})
+}
+
+$.ajax({
+	type:'POST',
+	url:listpost,//获取列表
+	timeout:10000,
+	data:{
+		pageNo:1,
+		pageSize:10,
 	},success:function(data){
 		$.each(data.list,function(index){
-			$('.listcontent').append('<div class="listlist"><ul><li>'+data.list[index].shopName+'</li><li></li><li></li><li></li><li></li><li></li><li></li><li></li><li class="shenhe">立即审核</li></ul></div>');
+			$('.listcontent').append('<div class="listlist"><ul><li>'+data.list[index].shopName+'</li><li>'+data.list[index].shopCode+'</li><li class="shenh shenhe">立即审核</li><li class="listid">'+ data.list[index].id +'</li></ul></div>');	
+		});
+		var listpagenum = Math.ceil(data.total/10);
+		
+		for(var i = 0; i<listpagenum; i++){
+			$('.listpage').append('<li>'+(i+1)+'</li>');
+		}
+			$('.listpage li').eq(0).addClass('cli');
+			var linum =$('.listpage li').length;
+			listpagebtn(linum);
+				if(linum>5){
+				for(var i = 5; i<linum; i++){
+					$('.listpage li').eq(i).hide();
+				}
+				}
+			
+			
+		$('.shenh').click(function(){
+				$('.list-dede').show();
+			var listid= $(this).siblings('.listid').html();
+
+			postlistmain(listid);
 			
 		});
-		
-		console.log(data);
-	}
+	},complete : function(XMLHttpRequest,status){ 
+　　　　if(status=='timeout'){
+ 　　　　　 ajaxTimeoutTest.abort();
+　　　　　  alert("超时");
+　　　　}
+　　}
 })
+function postlistmain(id){
+
+	$.ajax({
+		type:'POST',
+		url:contentpost,
+		data:{
+			id:id
+		},success:function(data){
+			var sucid = data.id;
+			$('.mdname').html(data.shopName);
+			$('.mdnames').html('门店名称: '+ data.shopName);
+			$('.address').html(data.shopAddress);
+			$('.dzdpname').html(data.publicCommentName);
+			
+			$('.mtname').html(data.meituanName);
+			$('.shengshiqu').html(data.province + data.city +data.area );
+			$('.zzimg img').attr('src',data.bussinessLicense);
+			$('.bhss').html(data.facilitie);
+			$('.yysj').html(data.businessTime);
+			$('.wsaq').html(data.healthSafe);
+			$('.wxts').html(data.warmPrompt);
+			$('.jtxx').html(data.trafficInformation);
+			$('.tcc').html(data.parkingInformation);
+			map(data.shopAddress);
+			$('.fmimg img').attr('src',data.shopCoverImag);
+			
+			$('#map-w').val(data.latitude);
+			$('#map-n').val(data.longitude);
+			$('.shoptel').html('电话 : ' +   data.shopTel);
+		//$('.mdimgs img').attr('src',);
+			var yourString=data.shopImag;
+			var result=yourString.split(",");
+			var imgarray = "";
+			for(var i=0; i<result.length; i++)
+			{
+				imgarray = imgarray +"<img  src='"+result[i]+"'>";
+			}
+			
+			$('.mdimgs').html(imgarray);
+			$('.list-de-right img').click(function(event){
+			event.stopPropagation();
+			$('.openimg').show();
+			$('.openimg img').attr('src',$(this).attr('src'));
+			});
+		
+
+		$('.pass').click(function(){
+			
+			$.ajax({
+				type:"POST",
+				url:uploadpost,
+				data:{
+					paramJson:jsons,
+					id:sucid
+				},success:function(data){
+					alert('审核成功');
+				}
+				
+			})
+			
+		})	
+		}
+	})
+	
+}
+
+
 
 })
